f9a301bff524b5ca2373df0d2549d800ea858784
Haoyuan Lin <specialemon@gmail.com>
2019-03-08 19:00:35 -0500

    react 1

---
+++ precache-manifest.80f6610a3d5d6f07d583112ff440e48c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf3a58057cad6769394b0951890708da",
+    "url": "/static/media/homePageBackground.bf3a5805.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "8f9e33a8955adcc20429",
+    "url": "/static/js/main.6f9bcac7.chunk.js"
+  },
+  {
+    "revision": "539306011173f10b5c30",
+    "url": "/static/js/2.85b1d063.chunk.js"
+  },
+  {
+    "revision": "8f9e33a8955adcc20429",
+    "url": "/static/css/main.e634dbbf.chunk.css"
+  },
+  {
+    "revision": "ccf9bb9bde0d4a2c109e569ef1ed392e",
+    "url": "/index.html"
+  }
+];
